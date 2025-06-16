c52b0e00183bd9f7fe7e71e4e734581969e5e1bc
Akshit Singhal <singhal.akshit@gmail.com>
2025-06-17 01:06:03 +0530

    introduction to react

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("div",{},[
+    React.createElement("h1",{id:"abc"},"hello"),
+    React.createElement("h1",{id:"xyz"},"bye"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
