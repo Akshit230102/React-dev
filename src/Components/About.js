4ce1bf93f3eee576f2cb000961282217a96967a5
Akshit Singhal <singhal.akshit@gmail.com>
2025-07-23 00:10:54 +0530

    created a class based component for user data

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,12 @@
+import User from "./User"
+import UserClass from "./UserClass"
+
 const About = () => {
   return (
     <div>
       <h1> About Us </h1>
+      <User name="Akshit Singhal" location="Delhi" contact="singhal.akshit@gmail.com"/>
+      <UserClass name="Akshit Singhal" location="Delhi" contact="singhal.akshit@gmail.com"/>
     </div>
   )
 }
