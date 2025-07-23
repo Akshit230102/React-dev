fd191f32e254f2bbbe738ad754d77958893a3c2b
Akshit Singhal <singhal.akshit@gmail.com>
2025-07-24 00:07:26 +0530

    used custom hook to fetch restaurant menu

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,43 +1,33 @@
-import { useState, useEffect } from "react";
-import { RES_MENU_URL } from "../Utils/constants";
 import { useParams } from "react-router";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../Utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  const resInfo = useRestaurantMenu(resId);
 
-  const {resId} = useParams();
+  if (resInfo === null) return <Shimmer />;
 
-  const fetchMenu = async () => {
-    const data = await fetch(RES_MENU_URL+resId);
-    const json = await data.json();
-    setResInfo(json);
-  };
-
-  if(resInfo === null) return <Shimmer/>;
-
-  const {name, cuisines, costForTwoMessage} = resInfo?.data?.cards[2]?.card?.card?.info;
-  const { itemCards } =
-    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-      ?.card?.categories[1] ||
-    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-      ?.card;
+  const { name, cuisines, costForTwoMessage } = resInfo?.data?.cards[2]?.card?.card?.info;
+  const { itemCards } = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
   return (
     <div>
-        <h1>{name}</h1>
-        <p>{cuisines.join(", ")} - {costForTwoMessage}</p> 
-        <h2> Menu</h2>
-        <ul>
-            {itemCards.map(item=> 
-            <li key={item?.card?.info?.id}>
-              <p>{item?.card?.info?.name} - Rs {item?.card?.info?.price/100 || item?.card?.info?.finalPrice/100} </p>
-            </li>)}
-        </ul>
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      <h2> Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id}>
+            <p>
+              {item?.card?.info?.name} - Rs. {item?.card?.info?.price / 100 || item?.card?.info?.finalPrice / 100}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
