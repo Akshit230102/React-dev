1d85b9f52323fa0439e7f1a5cf34c645f1eb6734
Akshit Singhal <singhal.akshit@gmail.com>
2025-07-23 21:24:41 +0530

    called github API in componentDidMount

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -4,23 +4,29 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        count: 0,
+        userInfo: {
+            name: "default",
+            id: "default",
+        },
     };
   }
+
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/Akshit230102");
+    const json = await data.json();
+    this.setState({
+        userInfo: json,
+    });
+  }
+
   render() {
-    const {name, location, contact}  = this.props;
-    const {count} = this.state;
+    const {name, id, avatar_url}  = this.state.userInfo;
+
     return (
       <div className="user-card">
-        <h1> Count: {count}</h1>
-        <button onClick={()=>{
-            this.setState({
-                count: count+1,
-            });
-        }}>Increase count</button>
-        <h2> Name: {name} </h2>
-        <h3> Location: {location}</h3>
-        <h3> Contact: {contact}</h3>
+        <img src={avatar_url} />
+        <h3> Name: {name}</h3>
+        <h3> Id: {id}</h3>
       </div>
     );
   }
